922a03dcd9b6bb5310c35ae58a775f8b0d12cd27
Omar Leiva <oleivahn@gmail.com>
2023-09-25 03:55:13 -0400

    New homepage

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import AboutUs from "./Components/AboutUs";
 import ContactForm from "./Components/ContactForm";
 import Hero from "./Components/Hero";
 import Navbar from "./Components/Navbar";
-import Slider from "./Components/Slider";
+// import Slider from "./Components/Slider";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <>
+      <HomePage />
       <Navbar />
       <Hero />
-      <Slider />
+      {/* <Slider /> */}
       <AboutUs />
       <ContactForm />
     </>
