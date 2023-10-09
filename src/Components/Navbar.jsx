5e91d79161e09974a2d557c2d9f44b4bbdaf1765
Omar Leiva <oleivahn@gmail.com>
2023-10-09 03:41:20 -0400

    Offset 2

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -35,7 +35,7 @@ function Navbar() {
               to="contactUs"
               // spy={true}
               smooth={true}
-              offset={-10}
+              offset={-30}
               duration={1000}
             >
               Contact Us
